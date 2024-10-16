8c94d6e0bbbff6d212e7682e214c5bf4a22e7c4b
ingriddeandradelopes <163137409+ingriddeandradelopes@users.noreply.github.com>
2024-10-16 09:20:45 -0300

    1 savar

---
+++ cript.js
@@ -0,0 +1,11 @@
+let valorCortador= 0;
+
+function incrementar(){
+    valorCortador++;
+    Document.getElementByld('contador').innrtext = valorCOntador;
+}
+
+function diminuir(){
+    valorCortador--;
+    document.getElementById('contador').innerTent = valorCortador;
+}
